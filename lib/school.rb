7a53e3ef1d95f17e341fd07a534d0d51cf4b2b8f
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-07-28 14:22:38 -0400

    solved school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
-# code here!
+
+require 'pry'
+class School
+
+  attr_accessor :name, :roster, :sorted_hash
+
+  def initialize(name=nil)
+    @name = name
+    @roster = {}
+    @sorted_hash = {}
+  end
+
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |grade, students|
+      new_hash[grade] = []
+      new_hash[grade] = students.sort
+    end
+    new_hash
+  end
+end
+
+Oxford = School.new("Oxford High School")
+
+Oxford.add_student("zed Slater", 9)
+Oxford.add_student("BB token", 9)
+Oxford.add_student("sally field", 3)
+
+
+# puts Oxford #object id
+# puts Oxford.roster
+
+Oxford.sort
